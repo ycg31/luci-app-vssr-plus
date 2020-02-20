e2467fd0959ea1bb71b3bad1194798121264c76e
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2020-02-20 22:40:33 +0800

    Update servers-list.lua

--- luasrc/model/cbi/vssr/servers-list.lua
+++ luasrc/model/cbi/vssr/servers-list.lua
@@ -26,9 +26,9 @@ s.addremove = true
 s.description = string.format(translate("Server Count") ..  ": %d", server_count)
 s.sortable = true
 s.template = "cbi/tblsection"
-s.extedit = luci.dispatcher.build_url("admin/vpn/vssr/servers/%s")
-function s.create(...)
-	local sid = TypedSection.create(...)
+s.extedit = d.build_url("admin", "vpn", "vssr", "servers", "%s")
+function s.create(e, t)
+	local sid = TypedSection.create(e, t)
 	if sid then
 		luci.http.redirect(s.extedit % sid)
 		return
