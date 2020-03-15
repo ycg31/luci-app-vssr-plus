13459208ff2b45d8b6937ce54da8594694a09aac
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2020-03-15 13:00:37 +0800

    Update status.lua

--- luasrc/model/cbi/vssr/status.lua
+++ luasrc/model/cbi/vssr/status.lua
@@ -272,8 +272,8 @@ t = m:section(Table, procs, translate("Running Details: ") .. "(/var/etc)")
 t:option(DummyValue, "PID", translate("PID"))
 t:option(DummyValue, "COMMAND", translate("CMD"))
 t:option(DummyValue, "LISTEN", translate("LISTEN"))
-t:option(DummyValue, "%CPU", translate("CPU"))
-t:option(DummyValue, "%MEM", translate("MEM"))
+t:option(DummyValue, "%CPU", translate("CPU usage (%)"))
+t:option(DummyValue, "%MEM", translate("Memory usage (%)"))
 
 s=m:field(DummyValue,"redir_run",translate("Global Client")) 
 s.rawhtml  = true
