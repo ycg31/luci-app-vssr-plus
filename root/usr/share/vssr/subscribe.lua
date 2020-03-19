b4ac5aab95396376570820e6fbb9c33d54f69e90
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2020-03-19 14:48:17 +0800

    Update subscribe.lua

--- root/usr/share/vssr/subscribe.lua
+++ root/usr/share/vssr/subscribe.lua
@@ -19,7 +19,7 @@ local cache = {}
 local nodeResult = setmetatable({}, { __index = cache })  -- update result
 local name = 'vssr'
 local uciType = 'servers'
-local ucic = uci.cursor()
+local ucic = luci.model.uci.cursor()
 local proxy = ucic:get_first(name, 'server_subscribe', 'proxy', '0')
 local switch = ucic:get_first(name, 'server_subscribe', 'switch', '1')
 local subscribe_url = ucic:get_first(name, 'server_subscribe', 'subscribe_url', {})
@@ -410,7 +410,6 @@ local execute = function()
 			                  luci.sys.call("/etc/init.d/" .. name ..  " restart > /dev/null 2>&1 &") 
 			
 		else
-			log('没有服务器节点了，停止服务')
 			luci.sys.call("/etc/init.d/" .. name .. " stop > /dev/null 2>&1 &") 
 		end
 		log('新增节点数量: ' ..add, '删除节点数量: ' .. del)
